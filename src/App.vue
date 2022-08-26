583a1b6fa0fac35d4e46c877780d46eb597606e3
DESKTOP-992SPOV\Gabriel Oliveira <gb.oliveirati@gmail.com>
2022-08-26 18:50:46 -0300

    I setup the project and also create the structure of the components

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <CardCategory />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import CardCategory from './components/CardCategory.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    CardCategory
+  }
+});
+</script>
+
+<style>
+    div#app {
+        background-color: rgba(255, 255, 255, .6);
+    }
+</style>
---
+++ src/components/CardCategory.vue
@@ -0,0 +1,31 @@
+<template>
+        <div class="card">
+            <h1>Perguntas frequentes</h1>
+            <small>Escolha a categoria desejada</small>
+            <ul class="lista">
+                <!-- <li class="item" v-for="categoria in categorias" :key="categoria.id>{{categoria}}</li> -->
+            </ul>
+        </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    
+})
+</script>
+
+<style scoped>
+   
+    .card {
+        border-radius: 15px;
+        background-color: #232323;
+        color: #fff;
+        display: grid;
+        justify-content: center;
+        align-content: center;
+        justify-items: center;
+        align-items: center;
+    }
+</style>
